c6db8c7e235a74705607772796be412dc7d02f15
Thomas Sweeney <tsweeney256@gmail.com>
2015-11-07 13:38:52 -0500

    no longer select idTitle

--- sql/basicSelect.sql
+++ sql/basicSelect.sql
@@ -1,7 +1,7 @@
-select pTitle.idName, series.idSeries, name as Title, rating as Rating, type as "Release Type", status as "Watched Status", year as Year, 
+select series.idSeries, name as Title, rating as Rating, type as "Release Type", status as "Watched Status", year as Year, 
 	season as Season, episodesWatched as "Episodes Watched", totalEpisodes as "Total Episodes", 
 	rewatchedEpisodes as "Rewatched Episodes" , episodeLength as "Episode Length", dateStarted as "Date Started", dateFinished as "Date Finished"
-from (select * from Title where primaryName = 1 and idLanguage = ?) as pTitle
+from (select * from Title inner join Label on Title.idLabel = Label.idLabel where Main = 1) as pTitle
 left join Series
 on pTitle.idSeries = series.idSeries
 left join ReleaseType
