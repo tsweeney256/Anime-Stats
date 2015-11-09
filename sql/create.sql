cb9b8a66eecd4499571f7ac87f4966e98032e5c2
Thomas Sweeney <tsweeney256@gmail.com>
2015-11-08 21:14:36 -0500

    added a blank name to the restricted values tables

--- sql/create.sql
+++ sql/create.sql
@@ -3,10 +3,11 @@ CREATE TABLE "WatchedStatus" (
   "idWatchedStatus" INTEGER PRIMARY KEY,
   "status" TEXT NOT NULL
 );
-INSERT INTO `WatchedStatus` VALUES (0,'Watched');
-INSERT INTO `WatchedStatus` VALUES (1,'Watching');
-INSERT INTO `WatchedStatus` VALUES (2,'Stalled');
-INSERT INTO `WatchedStatus` VALUES (3,'Dropped');
+INSERT INTO `WatchedStatus` VALUES (0,'');
+INSERT INTO `WatchedStatus` VALUES (1,'Watched');
+INSERT INTO `WatchedStatus` VALUES (2,'Watching');
+INSERT INTO `WatchedStatus` VALUES (3,'Stalled');
+INSERT INTO `WatchedStatus` VALUES (4,'Dropped');
 CREATE TABLE "Title" (
   "idName" INTEGER PRIMARY KEY,
   "name" TEXT NOT NULL,
@@ -15,12 +16,12 @@ CREATE TABLE "Title" (
   CONSTRAINT "fk_Title_Show1" FOREIGN KEY ("idSeries") REFERENCES "Series" ("idSeries") ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT "fk_Title_Label1" FOREIGN KEY ("idLabel") REFERENCES "Label" ("idLabel") ON DELETE SET DEFAULT ON UPDATE CASCADE
 );
-
 CREATE TABLE "Series" (
   "idSeries" INTEGER PRIMARY KEY,
   "rating" INTEGER DEFAULT NULL,
   "idReleaseType" INTEGER DEFAULT NULL,
   "idWatchedStatus" INTEGER DEFAULT NULL,
+  "idLengthType" INTEGER DEFAULT NULL,
   "year" INTEGER DEFAULT NULL,
   "idSeason" INTEGER DEFAULT NULL,
   "episodesWatched" INTEGER DEFAULT NULL,
@@ -34,24 +35,33 @@ CREATE TABLE "Series" (
   CONSTRAINT "fk_Show_Season1" FOREIGN KEY ("idSeason") REFERENCES "Season" ("idSeason") ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT "fk_Show_WatchedStatus1" FOREIGN KEY ("idWatchedStatus") REFERENCES "WatchedStatus" ("idWatchedStatus") ON DELETE NO ACTION ON UPDATE NO ACTION
 );
-
 CREATE TABLE "Season" (
   "idSeason" INTEGER PRIMARY KEY,
   "season" TEXT DEFAULT NULL
 );
-INSERT INTO `Season` VALUES (0,'Winter');
-INSERT INTO `Season` VALUES (1,'Spring');
-INSERT INTO `Season` VALUES (2,'Summer');
-INSERT INTO `Season` VALUES (3,'Fall');
+INSERT INTO `Season` VALUES (0,'');
+INSERT INTO `Season` VALUES (1,'Winter');
+INSERT INTO `Season` VALUES (2,'Spring');
+INSERT INTO `Season` VALUES (3,'Summer');
+INSERT INTO `Season` VALUES (4,'Fall');
 CREATE TABLE "ReleaseType" (
   "idReleaseType" INTEGER PRIMARY KEY,
   "type" TEXT NOT NULL
 );
-INSERT INTO `ReleaseType` VALUES (0,'TV');
-INSERT INTO `ReleaseType` VALUES (1,'OVA');
-INSERT INTO `ReleaseType` VALUES (2,'ONA');
-INSERT INTO `ReleaseType` VALUES (3,'Movie');
-INSERT INTO `ReleaseType` VALUES (4,'TV Special');
+INSERT INTO `ReleaseType` VALUES (0,'');
+INSERT INTO `ReleaseType` VALUES (1,'TV');
+INSERT INTO `ReleaseType` VALUES (2,'OVA');
+INSERT INTO `ReleaseType` VALUES (3,'ONA');
+INSERT INTO `ReleaseType` VALUES (4,'Movie');
+INSERT INTO `ReleaseType` VALUES (5,'TV Special');
+CREATE TABLE "LengthType" (
+  "idLengthType" INTEGER PRIMARY KEY,
+  "type" TEXT NOT NULL
+);
+INSERT INTO `LengthType` VALUES (0,'Normal');
+INSERT INTO `LengthType` VALUES (1,'Half');
+INSERT INTO `LengthType` VALUES (2,'Short');
+INSERT INTO `LengthType` VALUES (3,'Long');
 CREATE TABLE "Label" (
 	`idLabel`	INTEGER,
 	`label`	TEXT NOT NULL,
