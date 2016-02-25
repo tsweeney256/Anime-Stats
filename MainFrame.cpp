0f16e5f0bab0e3f1859c5ec2a49e5163d252aeda
Thomas Sweeney <tsweeney256@gmail.com>
2016-02-25 15:23:31 -0500

    removed OpenDbDlg as it is not needed anymore

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 #include "MainFrame.hpp"
 #include "DataPanel.hpp"
 #include "cppw/Sqlite3.hpp"
-#include "OpenDbDlg.hpp"
 #ifdef NDEBUG
 #include <iostream>
 #endif
