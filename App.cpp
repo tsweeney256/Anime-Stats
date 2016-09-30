df75f437909fdc5abbd8bb9685ec6be534c0a3a0
tsweeney256@gmail.com <tsweeney256@gmail.com>
2016-09-29 23:18:55 -0400

    Increased starting width of frame

--- App.cpp
+++ App.cpp
@@ -20,7 +20,7 @@ IMPLEMENT_APP(App); //tells wxWidgets where our main class for our app is
 
 bool App::OnInit()
 {
-    auto frame = new MainFrame("Anime Stats", wxPoint(50,50), wxSize(1024, 768));
+    auto frame = new MainFrame("Anime Stats", wxPoint(50,50), wxSize(1100, 768));
     frame->Show(true);
     return true;
 }
